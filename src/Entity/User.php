95d4a85893d09207237b8a3e33cfd8bad688acd5
Pewoh <burl410@gmail.com>
2022-01-21 15:30:36 +0100

    Configuration correct + Configuration route for User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,9 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    itemOperations: ['get', 'patch', 'delete']
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
