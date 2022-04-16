18f5e3fdb91c8b42f9a153894fe6f4732cd63f6c
Pewoh <burl410@gmail.com>
2022-04-16 12:45:02 +0200

    Add category entity + migration + Validator

---
+++ back/src/Entity/Category.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource(
+    itemOperations: ['get', 'patch', 'delete'],
+    normalizationContext: ['groups' => ['read']]
+)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[Groups(['read'])]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read'])]
+    #[Assert\NotBlank(message: 'name should be not blank')]
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
